b68f47e64ccf2d5b1f964ef046a6216be8a5e725
Steve-wt <57028143+Steve-wt@users.noreply.github.com>
2019-11-09 20:53:55 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,14 @@
+let i = 0;
+
+function bar1 () {
+	i = i + 1 ;
+    $('#success').width(i +'%');
+}
+function bar3 () {
+	i = i + 3 ;
+    $('#success').width(i +'%');
+}
+function bar7 () {
+	i = i + 7 ;
+    $('#success').width(i +'%');
+}
